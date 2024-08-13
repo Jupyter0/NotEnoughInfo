cb13a430514b872b6619266f5a55b3ede7a78c32
GitHub Action <action@github.com>
2024-08-13 23:20:42 +0000

    Initialize template repository proper
    Set package name to com.github.jupyter0.notenoughinfo
    Set project name to NotEnoughInfo
    Set modid to notenoughinfo

--- src/main/java/com/github/jupyter0/notenoughinfo/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/jupyter0/notenoughinfo/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.jupyter0.notenoughinfo.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
